44ed7b6062f2485fb6330e52f8f10dee8b1eff83
Ji Wang <wj42421999@gmail.com>
2022-03-29 21:57:21 -0400

    Implemented logout

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -36,6 +36,15 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
     }
 
+    @IBAction func onLogOut(_ sender: Any) {
+        PFUser.logOut()
+
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(identifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
+        delegate.window?.rootViewController = loginViewController
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         posts.count
     }
